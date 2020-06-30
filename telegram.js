41225ec4e240c70514915a5c962747652d16a568
Andriy <manokhin.kpi@gmail.com>
2020-06-30 04:43:25 +0300

    webhook attached

--- telegram.js
+++ telegram.js
@@ -91,10 +91,10 @@ function parseCountry($, countryName) {
 
 const API_TOKEN = process.env.TELEGRAM_TOKEN || '1114937560:AAEWwk_x9TLJ_qTiGg9P2dakFNA4Gmgq6_0';
 const PORT = process.env.PORT || 3000;
-const URL = process.env.URL || 'https://telegram-bot-hoster.herokuapp.com';
+const BOT_URL = process.env.URL || 'https://telegram-bot-hoster.herokuapp.com';
 
 const bot = new Telegraf(API_TOKEN);
-bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+bot.telegram.setWebhook(`${BOT_URL}/bot${API_TOKEN}`);
 bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
 
 bot.use(async (ctx, next) => {
